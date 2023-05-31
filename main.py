152da903b2dd34df30a5e98dd130e88921d8b1c9
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-30 21:54:34 -0400

    q1

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 print("Howdy, this is a choose your own adventure game! Make your selections based on the prompts to test your fate!\n")
+
+alarm = input("\"Beeeep Beeeep\" ugh theres your alarm, another day of work.. time to get up... or do you want to press snooze? enter \"G\" to get up or \"S\" to snooze: ")
